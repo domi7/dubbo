125af5ddd45ced49fde513b35710fd46cc881343
Administrator <Administrator@2013-20160726IU>
2017-05-30 12:06:03 +0800

    添加注释

--- dubbo-server/src/main/java/com/domi7/dubbo/server/CityDubboServiceImpl.java
+++ dubbo-server/src/main/java/com/domi7/dubbo/server/CityDubboServiceImpl.java
@@ -4,6 +4,12 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.domi7.dubbo.domain.City;
 import com.domi7.dubbo.service.CityDubboService;
 
+/**
+ * 服务实现类
+ * 
+ * @author domi7
+ * @since 2017年5月30日
+ */
 @Service(version = "1.0.0")
 public class CityDubboServiceImpl implements CityDubboService {
 	public City findCityByName(String cityName) {
--- dubbo-server/src/main/java/com/domi7/dubbo/server/ServerApplication.java
+++ dubbo-server/src/main/java/com/domi7/dubbo/server/ServerApplication.java
@@ -3,6 +3,12 @@ package com.domi7.dubbo.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 服务端启动类
+ * 
+ * @author domi7
+ * @since 2017年5月30日
+ */
 @SpringBootApplication
 public class ServerApplication {
 
